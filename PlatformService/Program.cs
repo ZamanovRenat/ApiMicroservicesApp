e82941d0297cf20fd3eb1cc70f88569483632fc1
ZamanovRenat <zamanov.renat@mail.ru>
2022-04-02 22:14:48 +0300

    Регистрация зависимостей репозитория

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connection));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
