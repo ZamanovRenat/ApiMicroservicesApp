82eb135d99a8441418c2fcddf71665f78f74ba71
ZamanovRenat <zamanov.renat@mail.ru>
2022-04-02 21:48:38 +0300

    Реализация интерфейса репозитория

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,49 @@
+﻿using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+        public Platform GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _context.Platforms.Add(platform);
+        }
+        public void UpdatePlatform(Platform platform)
+        {
+            _context.Platforms.Update(platform);
+        }
+        public void DeletePlatformById(int id)
+        {
+            Platform platform = _context.Platforms.FirstOrDefault(u => u.Id == id);
+            _context.Platforms.Remove(platform);
+            _context.SaveChanges();
+        }
+    }
+}
