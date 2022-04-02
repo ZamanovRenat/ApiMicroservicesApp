6d1f6bc12b961c3c2ef4716228c71773da16433e
ZamanovRenat <zamanov.renat@mail.ru>
2022-04-02 21:49:02 +0300

    Регистрция зависмостей строка подключения к БД SQLite

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connection));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
