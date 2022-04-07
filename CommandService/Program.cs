b3f7613e70dc4067a5cec43c612bd5f32404e5fc
ZamanovRenat <zamanov.renat@mail.ru>
2022-04-08 00:32:33 +0300

    Регистрация зависимостей на контекст БД и репозиторий

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,9 +1,14 @@
+using CommandService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
